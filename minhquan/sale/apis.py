2bafe349af32a47088e3626cbeec3dee29c1752e
Linh Pham <lpham@deloitte.ca>
2022-08-22 16:39:26 +0700

    publish apis

--- minhquan/sale/apis.py
+++ minhquan/sale/apis.py
@@ -1,13 +1,17 @@
 import json
 from datetime import datetime
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.sessions.models import Session
+
 from django.forms.models import model_to_dict
+from django.db.models import Q
 from django.http import JsonResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from .models import Order, Coupon, Partner, Product
+from .models import User, Address, Order, Coupon, Partner, Product, ProductCategory
 
 from . import services
 
@@ -246,6 +250,175 @@ def create_address(request):
   })
 
 
+# ###########################################################################
+@csrf_exempt
+@require_http_methods(['POST'])
+def auth_register(request):
+  try:
+    username = request.POST['username']
+    password = request.POST['password']
+    user = User.objects.get_or_create({ username, password })
+    return JsonResponse({
+      'success': True,
+      'data': user
+    })
+  except:
+    return JsonResponse({
+      'success': False,
+      'data': None
+    })
+  
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def auth_login(request):
+  username = request.POST['username']
+  password = request.POST['password']
+  user = authenticate(request, username=username, password=password)
+  if user is not None:
+      login(request, user)
+      # session = Session.objects.get(session_key=request.session.session_key)
+      return JsonResponse({
+        'success': True,
+        'data': user.id
+      })
+  else:
+    return JsonResponse({
+      'success': False,
+      'data': None
+    })
+  
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def auth_logout(request):
+  try:
+    logout(request)
+    return JsonResponse({
+      'success': True,
+      'data': {}
+    })
+  except:
+    return JsonResponse({
+      'success': False,
+      'data': None
+    })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_users(request):
+  body_unicode = request.body.decode('utf-8')
+  body = json.loads(body_unicode)
+  query = body['query']
+  kwargs = eval(query)
+  users = User.objects.filter(**kargs)
+  return JsonResponse({
+    'success': True,
+    'data': list(users.values())
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_categories(request):
+  categories = ProductCategory.objects.all()
+  return JsonResponse({
+    'success': True,
+    'data': list(categories.values())
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_category(request, pk):
+  category = ProductCategory.objects.get(pk=pk)
+  return JsonResponse({
+    'success': True,
+    'data': model_to_dict(category)
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_products(request):
+  body_unicode = request.body.decode('utf-8')
+  body = json.loads(body_unicode)
+  
+  category = body.get('category', None)
+  search_text = body.get('search_text', None)
+
+  filter_condition = Q()
+  if category:
+    filter_condition.add(Q(category_id=category), Q.AND)
+  if search_text:
+    filter_condition.add(Q(name__contains=search_text), Q.AND)
+  
+  products = Product.objects.filter(filter_condition)
+
+  result = []
+  for product in products:
+    product_model = model_to_dict(product, exclude=['image'])
+    product_model['image'] = request._current_scheme_host + product.image.url
+    result.append(product_model)
+
+  return JsonResponse({
+    'success': True,
+    'data': result
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_product(request, pk):
+  product = Product.objects.get(pk=pk)
+  response = model_to_dict(product, exclude=['image'])
+  response['image'] = request._current_scheme_host + product.image.url
+  return JsonResponse({
+    'success': True,
+    'data': response
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_product_by_slug(request, slug):
+  product = Product.objects.get(slug=slug)
+  response = model_to_dict(product, exclude=['image'])
+  response['image'] = request._current_scheme_host + product.image.url
+  return JsonResponse({
+    'success': True,
+    'data': response
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_carts(request):
+  body_unicode = request.body.decode('utf-8')
+  body = json.loads(body_unicode)
+  carts = Order.objects.filter()
+  return JsonResponse({
+    'success': True,
+    'data': list(carts.values())
+  })
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_addresses(request):
+  body_unicode = request.body.decode('utf-8')
+  body = json.loads(body_unicode)
+  address = Address.objects.filter()
+  return JsonResponse({
+    'success': True,
+    'data': list(address.values())
+  })
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def get_coupons(request):
+  body_unicode = request.body.decode('utf-8')
+  body = json.loads(body_unicode)
+  coupons = Coupon.objects.filter()
+  return JsonResponse({
+    'success': True,
+    'data': list(coupons.values())
+  })
+
 
 urlpatterns = [
   path('get-coupon/', get_coupon, name='get_coupon'),
@@ -258,4 +431,17 @@ urlpatterns = [
   path('get-customer-by-rule/', get_customer_by_rule, name='get_product_by_rule'),
   path('create-partner/', create_partner, name='create_partner'),
   path('create-address/', create_address, name='create_address'),
+
+  path('v1/auth/register', auth_register, name='auth_register'),
+  path('v1/auth/login', auth_login, name='auth_login'),
+  path('v1/auth/logout', auth_logout, name='auth_logout'),
+  path('v1/users', get_users, name='get_users'),
+  path('v1/categories', get_categories, name='get_categories'),
+  path('v1/category/<int:pk>', get_category, name='get_category'),
+  path('v1/products', get_products, name='get_products'),
+  path('v1/product/<int:pk>', get_product, name='get_product'),
+  path('v1/product/<slug:slug>', get_product_by_slug, name='get_product_by_slug'),
+  path('v1/carts', get_carts, name='get_carts'),
+  path('v1/addresses', get_addresses, name='get_addresses'),
+  path('v1/coupons', get_coupons, name='get_coupons'),
 ]
